7dcfcc624361a1d1ecb883e8b76ded0cf1793793
RohitSingh1369 <74597640+RohitSingh1369@users.noreply.github.com>
2021-01-05 15:08:27 +0530

    Add files via upload

--- sudokuCheck.cpp
+++ sudokuCheck.cpp
@@ -15,24 +15,66 @@ int checkRow(char maze[9][9],int row, char num)
 		}
 		else if(maze[row][j] == num){
 			cout << "\nError!!! Not Correct Number!\n";
-			return 0;
+			return 1;
+		}
+	}	
+return 0;
+}
+
+int checkColumn(char maze[9][9],int column, char num)
+{
+	int k=0;
+	for(int i=0; i<9; i++)
+	{
+		if(maze[i][column] == '.'){
+			continue;
+		}
+		else if(maze[i][column] == num){
+			cout << "\nError!!! Not Correct Number!\n";
+			return 1;
+		}
+	}	
+return 0;
+}
+
+int checkSquare(char maze[9][9],int row, int column, char num)
+{
+	int r_strt = 3*(row/3);
+	int c_strt = 3*(column/3);
+	int k=0;
+	for(int i = r_strt; i < r_strt+3; i++)
+	{
+		for(int j = c_strt; j < c_strt+3; j++)
+		{
+			if(maze[i][j] == '.'){
+				continue;
+			}
+			else if(maze[i][j] == num){
+				cout << "\nError!!! Not Correct Number!\n";
+				return 1;
+			}	
 		}
 	}
-	
-	
-	
-
+		
+return 0;
 }
 
 void checkSudoku(char maze[9][9],int row,int column, char num)
 {
 	if(maze[row][column] == '.')
 	{
-		if(checkRow(maze,row,num)==0){
+		if(checkRow(maze,row,num)==1){
 			return;
 		}
-			
 		
+		if(checkColumn(maze,column,num)==1){
+			return;
+		}	
+		
+		if(checkSquare(maze,row,column,num) == 1){
+			return;
+		}
+		cout<<"\nEntered Number is Correct\n";
 	}
 	else
 	{
@@ -46,7 +88,7 @@ int main()
 {
     
     ifstream in;
-    in.open("maze.txt");
+    in.open("sudoku3.txt");
     if(!in)
     {
         cout<<"File not opened";
@@ -62,7 +104,7 @@ int main()
             in>>maze[i][j];
         }
     }
-    cout<<"MAZE"<<endl;
+    cout<<"SUDOKU:"<<endl;
     for(int i=0;i<rw;i++)
     {
         for(int j=0;j<col;j++)
@@ -75,12 +117,12 @@ int main()
     
     int row, column;
     char num;
-	    cout << "\nAgain Select The Empty Box row, column: " ;
+	    cout << "\nSelect The Empty Box row, column: " ;
 	    cin >> row;
 	    cin >> column;
 	    cout << "\nEnter the Number You Want to Fill: ";
     	    cin >> num; 
     	    
-    	    checkSudoku(maze,row,column,num);
-
+    checkSudoku(maze,row,column,num);
+return 0;
 }
